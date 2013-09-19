3b41ef8ff211d1e9b8f11e094eae79094efc0fb0
Diguet Jonathan <diguet.jonathan@gmail.com>
2013-09-19 09:40:07 -0300

    implementing rhino first attempt

--- ServicePushGap/src/com/example/hellogap/MyService.java
+++ ServicePushGap/src/com/example/hellogap/MyService.java
@@ -17,53 +17,68 @@ import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 
 import org.mozilla.javascript.*;
 
+
 public class MyService extends BackgroundService {
-
+	/*private static final String RHINO_LOG = "var log = Packages.io.vec.ScriptAPI.log;";
+	public static void log(String msg) {
+	    android.util.Log.i("RHINO_LOG", msg);
+	}
 	private final static String TAG = MyService.class.getSimpleName();
-
+*/
 	private String mHelloTo = "World";
 
-    private String jsstr="console.log(\"var text = 'Hello Android!\\\\nThis is JavaScript in action!';\\n\")";
+    private String jsstr="function hello(java) { if (typeof log != 'undefined') { log(\"JavaScript say hello to \" + java); log(\"Also, I can access Java object: \" + javaContext);} return { foo: \"bar in JavaScript\" };}";
+  // public JSONObject jobj=new JSONObject();
 
-
-	@SuppressWarnings("finally")
+	
 	@Override
 	protected JSONObject doWork() {
+		
 		JSONObject result = new JSONObject();
-
+	/*	try{
+		jobj.put("hello", jsstr);
+		}
+		catch(JSONException e){}
 		//try {
+		 String functionName = "hello";
+		 Object[] functionParams = new Object[] { "Android" };
             // Create an execution environment.
-            org.mozilla.javascript.Context cx =  org.mozilla.javascript.Context.enter();
+           org.mozilla.javascript.Context cx =  org.mozilla.javascript.Context.enter();
 
             // Turn compilation off.
             cx.setOptimizationLevel(-1);
-
+*/
             try
             {
                 // Initialize a variable scope with bindnings for
                 // standard objects (Object, Function, etc.)
-                Scriptable scope = cx.initStandardObjects();
+            /*    Scriptable scope = cx.initStandardObjects();
 
                 // Set a global variable that holds the activity instance.
-                ScriptableObject.putProperty(
-                        scope, "TheActivity",  org.mozilla.javascript.Context.javaToJS(this, scope));
+                ScriptableObject.putProperty(scope, "javaContext",  org.mozilla.javascript.Context.javaToJS(this.getClassLoader(), scope));
+
 
                 // Evaluate the script.
-                cx.evaluateString(scope, jsstr, "doti:", 1, null);
-            	
+                Object res= cx.evaluateString(scope, RHINO_LOG + jsstr, "MyService", 1, null);
+                Function function = (Function) scope.get(functionName, scope);
+                
+                NativeObject res1 = (NativeObject) function.call(cx, scope, scope, functionParams);
+                
+                String foo = (String) org.mozilla.javascript.Context.jsToJava(res1.get("foo", scope), String.class);
+                log(foo);*/
     			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
     			String now = df.format(new Date(System.currentTimeMillis())); 
 
-    			String msg = "Hello " + this.mHelloTo + " - its currently " + now;
+    			String msg = "Hello " + this.mHelloTo + " - its currently " + now ;
     			result.put("Message", msg);
-
-    			Log.d(TAG, msg);
+    			doit("return \"something\";");
+    			Log.d("msg", msg);
     		} catch (JSONException e) {
-
+    			//org.mozilla.javascript.Context.exit();
     			return result;	}
             finally
             {
-            	 org.mozilla.javascript.Context.exit();
+            //	org.mozilla.javascript.Context.exit();
             	 return result;
             }
 			
@@ -72,6 +87,44 @@ public class MyService extends BackgroundService {
 		
 
 }
+
+	private static final String RHINO_LOG = "var log = Packages.io.vec.ScriptAPI.log;";
+	public static void log(String msg) {
+	    android.util.Log.i("RHINO_LOG", msg);
+	}
+
+	   
+	    
+	    
+	    void doit(String code)
+	    {
+	        // Create an execution environment.
+	        org.mozilla.javascript.Context cx = org.mozilla.javascript.Context.enter();
+
+	        // Turn compilation off.
+	        cx.setOptimizationLevel(-1);
+
+	        try 
+	        {
+	            // Initialize a variable scope with bindnings for  
+	            // standard objects (Object, Function, etc.)
+	            Scriptable scope = cx.initStandardObjects();
+
+	            // Set a global variable that holds the activity instance.
+	            ScriptableObject.putProperty(
+	                scope, "TheActivity", org.mozilla.javascript.Context.javaToJS(this, scope));
+
+	            // Evaluate the script.
+	            Object result= cx.evaluateString(scope, code, "doit:", 1, null);    
+	            Log.d("resultJavascipt",cx.toString(result));
+	            	
+	            Log.d("msg","inside rhino routine");
+	        } 
+	        finally 
+	        {
+	        	org.mozilla.javascript.Context.exit();
+	        }
+	    }
 
 	@Override
 	protected JSONObject getConfig() {
