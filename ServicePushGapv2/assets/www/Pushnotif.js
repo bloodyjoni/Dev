8b78d931e0fda1e0070e94a4c15bb0bdf388d09c
bloodyjoni <diguet.jonathan@gmail.com>
2013-09-30 13:43:57 -0300

    Rebase

---
+++ ServicePushGapv2/assets/www/Pushnotif.js
@@ -0,0 +1,98 @@
+
+
+var PushConf = {
+		 addTokenScript : "http://www.proyectored.com.ar/mobile/addtoken.php?token=",
+		 deleteTokenScript  : "http://www.proyectored.com.ar/mobile/deletetoken.php?token=",
+		 senderID : "447745035223",
+		 token : "0" ,
+		// Application Constructor
+		 initialize : function() {
+			this.bindEvents();
+		},
+		// Bind Event Listeners
+		//
+		// Bind any events that are required on startup. Common events are:
+		// 'load', 'deviceready', 'offline', and 'online'.
+		 bindEvents : function() {
+			document.addEventListener('deviceready', onDeviceReadyforPush, false);
+			//onDeviceReadyforPush();
+		}
+};
+
+
+
+
+function onDeviceReadyforPush() {
+			pushNotification = window.plugins.pushNotification;
+			pushRegister();
+			// used to unregister the device
+			document.addEventListener("backbutton", onBackButton, false);
+
+
+		}
+
+
+			
+			
+function pushRegister(){
+
+	pushNotification.register(successHandler, errorHandler,{"senderID":PushConf.senderID,"ecb":"onNotificationGCM"});
+
+}
+
+function getXMLHttpRequest(){
+	var xhr = null;
+
+			xhr = new XMLHttpRequest(); 
+		
+
+	return xhr;
+}
+
+function  onNotificationGCM(e) {
+	switch( e.event )
+	{
+	case 'registered':
+		if ( e.regid.length > 0 )
+		{		}
+		break;
+
+	case 'message':
+		// this is the actual push notification. its format depends on the data model from the push server
+		alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+		break;
+
+	case 'error':
+		alert('GCM error = '+e.msg);
+		break;
+
+	default:
+		alert('An unknown GCM event has occurred');
+	break;
+	}
+}
+function onBackButton(e){
+
+	if (e){
+		e.preventDefault();
+
+	}
+	else
+	{
+		navigator.app.backHistory();
+	}
+
+
+}
+function errorHandler(error) {
+	alert(error);
+}
+	 function successHandler(e) {
+
+
+			console.log(result);
+
+		}
+		
+
+
---
+++ ServicePushGapv2/src/com/example/hellogap/ShutdownReciever.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.example.hellogap;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * @author Matilde
+ *
+ */
+public class ShutdownReciever extends BroadcastReceiver {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		
+		
+		unregisterBackground();
+	
+			
+	
+		
+	}
+	private void unregisterBackground() {
+	    new AsyncTask<Object, Object, Object>() {
+	        protected String doInBackground(Object... params) {
+	        	GoogleCloudMessaging gcm = null;			
+	        	SharedPreferences shrprfs= ApplicationContext.getContext().getSharedPreferences("SERVPUSH_PREFS", 0);
+	    		
+	    		String regid = shrprfs.getString("Regid", "regid missing");
+	    		
+				
+	        	String msg = "";
+	         
+	                if (gcm == null) 
+	                    gcm = GoogleCloudMessaging.getInstance(ApplicationContext.getContext());
+	                	try {
+	                		gcm.register("447745035223");
+							gcm.unregister();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							Log.d("ShutdownunregistrationUnregister","unregisterError");
+							e.printStackTrace();
+						}
+	                	
+	                	URL url = null;
+	            		try {
+	            			url = new URL("http://www.proyectored.com.ar/mobile/deletetoken.php?token="+regid);
+	            		} catch (MalformedURLException e) {
+	            			// TODO Auto-generated catch block
+	            			Log.d("Shutdownunregistration","InvalidURL");
+	            			e.printStackTrace();
+	            		}
+	            		HttpURLConnection urlConnection = null;
+	            		try {
+	            			urlConnection = (HttpURLConnection) url.openConnection();
+
+	            			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+	            			InputStreamReader reader= new InputStreamReader(in);
+	            			Log.d("urlconnection return",reader.toString());
+	            		} catch (Exception e) {
+	            			// TODO Auto-generated catch block
+	            			e.printStackTrace();
+	            			Log.d("ShutdownServiceExecp",e.toString());
+	            		}
+
+	            		finally {
+	            			urlConnection.disconnect();
+	            		}
+	            		Log.d("ShutDownReceiver","insideshutdwnreceiver");
+	            		return regid;
+	        }
+	    }.execute(null, null, null);
+	}
+}
